f8d7871a8c0b476909c1f775fcc14d2feb3b6df0
vitaly231 <fursov231@Hotmail.com>
2021-06-28 10:31:57 +0300

    first commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,23 @@
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
+const TokenFarm = artifacts.require("TokenFarm");
+
+module.exports = async function(deployer, network, accounts) {
+  //Deploy DAI Token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  //Deploy Dapp Token
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  //Deploy Farm Token
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed();
+
+  //Transfer all tokens to TokenFarm (1 million)
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000');
+
+  //Transfer 100 Mock DAI tokens to inverstor
+  await daiToken.transfer(accounts[1], '100000000000000000000');
+};
